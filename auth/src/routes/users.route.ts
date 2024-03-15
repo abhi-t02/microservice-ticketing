d3ddce8f2b0a0552c42a24ce1e454359b88f5c77
Abhi trivedi <abhi.t@crestinfosystems.com>
2024-03-15 17:09:29 +0530

    user sign out route added

--- auth/src/routes/users.route.ts
+++ auth/src/routes/users.route.ts
@@ -4,6 +4,7 @@ import validate from "../middleware/validateResources.middleware";
 import { signinUserSchema, signupUserSchema } from "../schema/users.schema";
 import {
   signinUserHandler,
+  signoutUserHandler,
   signupUserHandler,
 } from "../controller/users.controller";
 import { currentUserHandler } from "../controller/current-user.controller";
@@ -12,6 +13,7 @@ const router = Router();
 
 router.post("/signup", validate(signupUserSchema), signupUserHandler);
 router.post("/signin", validate(signinUserSchema), signinUserHandler);
+router.post("/signout", signoutUserHandler);
 router.get("/currentUser", currentUserHandler);
 
 export { router as userRouter };
